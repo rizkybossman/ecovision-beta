95fbf93261a9b234b4885ebd50e832a3390a2ae1
rizkybossman <rizky.fauzi2202@gmail.com>
2025-06-11 06:46:15 +0700

    my picture man

--- src/components/Team.jsx
+++ src/components/Team.jsx
@@ -58,7 +58,7 @@ const teamMembers = [
     name: "Rizky Fauzi",
     role: "Frontend & Backend Developer",
     description: "I`m just a Regular Everyday Normal Dude.",
-    image: "/images/Rizky.JPG",
+    image: "/images/Rizky.jpg",
     color: "bg-red-500",
     linkedin: "https://www.linkedin.com/in/rizkyfauzi-r8055f",
     animation: {
